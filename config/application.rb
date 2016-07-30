6ad6c97f7a7e972104405ca6e2e0fed524d8570b
irevived1 <haifeng.wu@jjay.cuny.edu>
2016-07-29 21:02:52 -0400

    Done.

--- config/application.rb
+++ config/application.rb
@@ -10,13 +10,13 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Arlab
   class Application < Rails::Application
+    config.web_console.development_only = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
